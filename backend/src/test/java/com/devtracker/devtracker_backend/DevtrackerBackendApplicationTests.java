6413b6e222cccafe8557fd01098ccf75a4b61193
chedi-debiche <ch.debiche@gmail.com>
2025-11-27 14:19:45 +0100

    initial commit : backend + frontend structure

---
+++ backend/src/test/java/com/devtracker/devtracker_backend/DevtrackerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devtracker.devtracker_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevtrackerBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
